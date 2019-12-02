0b44aae02cff8db7a5bb8ec78699fac03dfc8170
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-12-02 10:58:31 +0100

    Added server port fallback to 8081 if 8080 already in use

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ var app = express();
 
 app.use(express.static('.'))
 
-app.listen(8080, console.log('Server started.\n Open: http://localhost:8080'));
+try{
+    app.listen(8080, console.log('Server started.\n Open: http://localhost:8080'));
+}catch(e){
+    app.listen(8081, console.log('Server started.\n Open: http://localhost:8081'));
+}
